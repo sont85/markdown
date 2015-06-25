3829d239193b431d33868749a65094bf3cb80ce2
Son Truong <sont85@gmail.com>
2015-06-24 23:37:09 -0700

    markDOWN

---
+++ ghost.js
@@ -0,0 +1,29 @@
+var marked = require('marked'),
+    http = require("http"),
+    fs = require("fs");
+
+// console.log(marked('I am using __markdown__.'));
+
+http.createServer(responseHandler).listen(7777);
+
+function responseHandler(req, res) {
+  var markedBinding = ""
+  if (req.url.match("fav")) {
+    res.end("");
+    return;
+  }
+  res.writeHead(200, {"Content-Type": "text/html"});
+  if (req.url === "/") {
+    fs.readFile("index.html", "utf8", function(err,data) {
+      res.end(data);
+    });
+  } else if (req.url.match("/markdown/")) {
+    var markedText = req.url.replace("/markdown/", "")
+    markedText = marked(decodeURIComponent(markedText));
+    console.log(markedText)
+    res.end(markedText)
+  } else {
+    res.end()
+  }
+
+};
